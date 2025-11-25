448ff6c44bebdec219c1369f47786836750a27f5
Christopher Stingl <cstingl@protonmail.com>
2025-11-25 18:39:40 -0500

    2025: Prep for this years AoC

---
+++ 2025/daily-template/src/part2.rs
@@ -0,0 +1,16 @@
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 00 - part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        todo!("TODO: Build Test");
+        let input = "";
+        assert_eq!("", process(input)?);
+        Ok(())
+    }
+}
