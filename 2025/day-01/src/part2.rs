b2d5c385d5fd6ce8e17e6f27b7c2b73bde5f61ae
Wolves <cstingl@protonmail.com>
2025-12-01 10:48:04 -0500

    day-01(part2): Initial successful correct solution

--- 2025/day-01/src/part2.rs
+++ 2025/day-01/src/part2.rs
@@ -1,5 +1,45 @@
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 2");
+const STARTING_POINT: i32 = 50;
+
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut current_pos = STARTING_POINT;
+    let mut zero_count = 0;
+    for line in input.lines() {
+        let (dir, count_str) = line.split_at(1);
+        let count = count_str.parse::<i32>().unwrap();
+
+        let new_pos = match dir {
+            "L" => current_pos - count,
+            "R" => current_pos + count,
+            _ => unreachable!(),
+        };
+
+        let mut cycles = match dir {
+            "L" => {
+                current_pos.div_euclid(100)
+                    - new_pos.div_euclid(100)
+            }
+            "R" => {
+                new_pos.div_euclid(100)
+                    - current_pos.div_euclid(100)
+            }
+            _ => unreachable!(),
+        };
+
+        if dir == "L" {
+            if new_pos.rem_euclid(100) == 0 {
+                cycles += 1;
+            }
+
+            if current_pos == 0 && new_pos < 0 {
+                cycles -= 1;
+            }
+        }
+
+        zero_count += cycles;
+        current_pos = new_pos.rem_euclid(100);
+    }
+
+    Ok(zero_count.to_string())
 }
 
 #[cfg(test)]
@@ -8,9 +48,49 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        assert_eq!("6", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_for_r_calculates_0_crosses(
+    ) -> miette::Result<()> {
+        let input = "R1000";
+        assert_eq!("10", process(input)?);
+        Ok(())
+    }
+    #[test]
+    fn test_process_for_l_calculates_0_crosses(
+    ) -> miette::Result<()> {
+        let input = "L200";
+        assert_eq!("2", process(input)?);
+        Ok(())
+    }
+    #[test]
+    fn test_process_for_L_rotations_that_end_on_0(
+    ) -> miette::Result<()> {
+        let input = "L50";
+        assert_eq!("1", process(input)?);
+        Ok(())
+    }
+    #[test]
+    fn test_process_for_L_rotations_that_start_on_0(
+    ) -> miette::Result<()> {
+        let input = "L50
+L5
+R5
+L5";
+        assert_eq!("2", process(input)?);
         Ok(())
     }
 }
