cee985343aaa473821451996e0e4d4b1aefce9a1
Christopher Stingl <cstingl@protonmail.com>
2025-12-01 07:00:22 -0500

    day-01: Initial setup

---
+++ 2025/day-01/src/part1.rs
@@ -0,0 +1,26 @@
+pub fn process(_input: &str) -> miette::Result<String> {
+    let result = 0;
+
+    Ok(result.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        assert_eq!("3", process(input)?);
+        Ok(())
+    }
+}
