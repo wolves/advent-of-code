76909796f47ef2f41188f073399dfdd109b39f78
Wolves <cstingl@protonmail.com>
2025-12-03 10:26:53 -0500

    day-03(part1): Working solution

--- 2025/day-03/src/part1.rs
+++ 2025/day-03/src/part1.rs
@@ -1,13 +1,44 @@
-#[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 1");
+use itertools::Itertools;
+use tracing::info;
+
+#[tracing::instrument(skip(input))]
+pub fn process(input: &str) -> miette::Result<String> {
+    let input = input.trim();
+    let result = input
+        .lines()
+        .map(|bank| {
+            let (index, first_max) = &bank
+                [..(bank.len() - 1)]
+                .chars()
+                .enumerate()
+                .max_set_by_key(|(_index, battery)| {
+                    *battery
+                })
+                .first()
+                .cloned()
+                .unwrap();
+
+            let (_second_index, second_max) = &bank
+                [(index + 1)..]
+                .chars()
+                .enumerate()
+                .max_by_key(|(_index, battery)| *battery)
+                .unwrap();
+
+            format!("{first_max}{second_max}")
+                .parse::<u64>()
+                .unwrap()
+        })
+        .sum::<u64>();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    #[test_log::test]
     fn test_process() -> miette::Result<()> {
         let input = "987654321111111
 811111111111119
