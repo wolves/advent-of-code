492c4a643c3fd781d8f125db8631e629eaf9cd74
Wolves <cstingl@protonmail.com>
2025-12-04 08:28:52 -0500

    day-04(part1): Working solution and dupe to part 2

--- 2025/day-04/src/part2.rs
+++ 2025/day-04/src/part2.rs
@@ -1,17 +1,70 @@
-#[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 00 - part 2");
+use std::collections::HashSet;
+
+use glam::IVec2;
+use tracing::info;
+
+const NEIGHBORS: [IVec2; 8] = [
+    IVec2::X,          // South
+    IVec2::Y,          // East
+    IVec2::NEG_X,      // West
+    IVec2::NEG_Y,      // North
+    IVec2::ONE,        // SouthEast
+    IVec2::NEG_ONE,    // NorthWest
+    IVec2::new(1, -1), // NorthEast
+    IVec2::new(-1, 1), // SouthWest
+];
+
+#[tracing::instrument(skip(input))]
+pub fn process(input: &str) -> miette::Result<String> {
+    let input = input.trim();
+
+    let positions = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(
+                move |(x, value)| {
+                    (value == '@').then_some(IVec2::new(
+                        x as i32, y as i32,
+                    ))
+                },
+            )
+        })
+        .collect::<HashSet<IVec2>>();
+
+    let count = positions
+        .iter()
+        .filter(|&position| {
+            NEIGHBORS
+                .iter()
+                .filter(|&offset| {
+                    positions.contains(&(position + offset))
+                })
+                .count()
+                < 4
+        })
+        .count();
+
+    Ok(count.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    #[test_log::test]
     fn test_process() -> miette::Result<()> {
-        todo!("TODO: Build Test");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+        assert_eq!("43", process(input)?);
         Ok(())
     }
 }
